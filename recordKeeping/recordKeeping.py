925255e6bb2bbf4838c47f1fe68e7ee6d0f054cd
HashingSlash <dcjpreece@protonmail.com>
2023-02-09 15:45:34 +0800

    0.0.3 - txn Types
    parse transactions types,
    count each
    print to inform user

--- recordKeeping/recordKeeping.py
+++ recordKeeping/recordKeeping.py
@@ -27,27 +27,40 @@ except IOError: #database load failed. prompt user to input wallet address to in
 #   collect transactions using requestHandling module
 #   count existing stored txns, add received data, then count total stored txns.
 
-#Could use improvement.
-incomingTxns = requestHandling.reqTxns(db['wallet'], 'first')
-#--     return {'txns':txnJson['transactions'],'token':token}
-preRunCount = len(db['rawTransactions'])
-db['rawTransactions'] = requestHandling.addNewTxns(incomingTxns, db['rawTransactions'])
-#--     returns txnDB with new txns added to 
-newCount = len(db['rawTransactions']) - preRunCount
-while newCount != 0:
-    incomingTxns = requestHandling.reqTxns(db['wallet'], incomingTxns['token'])
+skipTxnFetch = False
+def fetchTxns():
+    #Could use improvement.
+    incomingTxns = requestHandling.reqTxns(db['wallet'], 'first')
+    #--     return {'txns':txnJson['transactions'],'token':token}
     preRunCount = len(db['rawTransactions'])
     db['rawTransactions'] = requestHandling.addNewTxns(incomingTxns, db['rawTransactions'])
+    #--     returns txnDB with new txns added to 
     newCount = len(db['rawTransactions']) - preRunCount
-else: print('Finished fetching transactions.\n')
+    while newCount != 0:
+        incomingTxns = requestHandling.reqTxns(db['wallet'], incomingTxns['token'])
+        preRunCount = len(db['rawTransactions'])
+        db['rawTransactions'] = requestHandling.addNewTxns(incomingTxns, db['rawTransactions'])
+        newCount = len(db['rawTransactions']) - preRunCount
+    else: print('Finished fetching transactions.\n')
 
+
+if len(db['rawTransactions']) == 0 or (skipTxnFetch == False and input('Would you like to request a transaction update? (Y/N): ').upper() == 'Y'):
+    fetchTxns()
+else: print('Skipping update')
 
 ####                Populate local dbs
+payC = 0
+keyregC = 0
+acfgC = 0
+axferC = 0
+afrzC = 0
+applC = 0
+
 groupedTxns = 0
 for txn in db['rawTransactions']:
-    ##      txn Rounds
+    ##                  txn Rounds
+
     txnRound = str(db['rawTransactions'][txn]['confirmed-round'])
-    
     if txnRound in db['txnRounds'] and txn not in db['txnRounds'][txnRound]:
     #If this txn Round is already in the DB but this txn is missing, add it.
         roundList = db['txnRounds'][txnRound]
@@ -57,7 +70,8 @@ for txn in db['rawTransactions']:
     #Add this txns Round and this txn to that round
         db['txnRounds'][txnRound] = [txn]
 
-    ##      txn Groups
+    ##                  txn Groups
+
     if 'group' in db['rawTransactions'][txn]: #filter out ungrouped txns
         groupedTxns += 1
         groupID = db['rawTransactions'][txn]['group']
@@ -70,9 +84,32 @@ for txn in db['rawTransactions']:
         #New group, add it and this txn.
             db['groups'][groupID] = [txn]
 
+    ##                  txn Types
+
+        if db['rawTransactions'][txn]['tx-type'] == 'pay':
+            txnDetails = db['rawTransactions'][txn]['payment-transaction']
+            payC += 1
+        elif db['rawTransactions'][txn]['tx-type'] == 'keyreg':
+            txnDetails = db['rawTransactions'][txn]['keyreg-transaction']
+            keyregC += 1
+        elif db['rawTransactions'][txn]['tx-type'] == 'acfg':
+            txnDetails = db['rawTransactions'][txn]['asset-config-transaction']
+            acfgC += 1
+        elif db['rawTransactions'][txn]['tx-type'] == 'axfer':
+            txnDetails = db['rawTransactions'][txn]['asset-transfer-transaction']
+            axferC += 1
+        elif db['rawTransactions'][txn]['tx-type'] == 'afrz':
+            txnDetails = db['rawTransactions'][txn]['asset-freeze-transaction']
+            afrzC += 1
+        elif db['rawTransactions'][txn]['tx-type'] == 'appl':
+            txnDetails = db['rawTransactions'][txn]['application-transaction']
+            applC += 1
+        else:
+            print('!!!txnDetails!!!')
+
 
 ####                Print interesting data
-print('Wallet Stats:')
+print('\nWallet Stats:')
 print('Total transactions: ' + str(len(db['rawTransactions'])))
 print('Number of rounds containing transactions: ' + str(len(db['txnRounds'])))
 print('Average number of transactions per round: ' + str(len(db['rawTransactions']) / len(db['txnRounds'])))
@@ -80,6 +117,13 @@ print('Number of singular transations: ' + str(len(db['rawTransactions']) - grou
 print('Number of grouped transactions: ' + str(groupedTxns))
 print('Number of transction groups: ' + str(len(db['groups'])))
 print('Average number of transactions per group: ' + str(groupedTxns / len(db['groups'])))
+print('\nTransaction types:')
+print('Payments: ' + str(payC))
+print('Key Regs: ' + str(keyregC))
+print('Asset Configs: ' + str(acfgC))
+print('Asset Transfers: ' + str(axferC))
+print('Asset Freezes: ' + str(afrzC))
+print('Application Transactions: ' + str(applC))
 
 ####                Store data
 dbJson = json.dumps(db)
